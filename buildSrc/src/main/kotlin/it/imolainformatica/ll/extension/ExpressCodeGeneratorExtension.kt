8c0664f705824e809aee3879e4ba0c508b2e026e
Alex Speranza <asperanza@imolainformatica.it>
2023-04-26 17:11:38 +0200

    feat: add some properties to the extension

--- buildSrc/src/main/kotlin/it/imolainformatica/ll/extension/ExpressCodeGeneratorExtension.kt
+++ buildSrc/src/main/kotlin/it/imolainformatica/ll/extension/ExpressCodeGeneratorExtension.kt
@@ -1,4 +1,8 @@
 package it.imolainformatica.ll.extension
 
 open class ExpressCodeGeneratorExtension {
+    var baseAppSourceFolder = "./src"
+    var mainRouteFileName = "main.ts"
+    var routesFolder = "routes"
+    var controllerFolder = "controllers"
 }
