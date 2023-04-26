68676d51b12a62ac5d6aa3a24a72498f89ac31ed
Alex Speranza <asperanza@imolainformatica.it>
2023-04-26 18:27:38 +0200

    feat: add storage of handlers for a route

--- buildSrc/src/main/kotlin/it/imolainformatica/ll/spec/RouteSpec.kt
+++ buildSrc/src/main/kotlin/it/imolainformatica/ll/spec/RouteSpec.kt
@@ -1,5 +1,32 @@
 package it.imolainformatica.ll.spec
 
+import java.util.*
+
 open class RouteSpec(val routePath: String) {
     var resourceName = ""
+
+    var genericHandlers: MutableMap<String, String> = mutableMapOf()
+        private set
+
+    var specificHandlers: MutableMap<String, String> = mutableMapOf()
+        private set
+
+    fun getAll(lazyBody: () -> String) = setGenericHandler("getEvery", lazyBody)
+    fun post(lazyBody: () -> String) = setGenericHandler("post", lazyBody)
+    fun getOne(lazyBody: () -> String) = setSpecificHandler("get", lazyBody)
+    fun put(lazyBody: () -> String) = setSpecificHandler("put", lazyBody)
+    fun delete(lazyBody: () -> String) = setSpecificHandler("delete", lazyBody)
+
+    private fun setGenericHandler(handlerBaseName: String, lazyBody: () -> String) =
+        setHandler(genericHandlers, handlerBaseName, lazyBody)
+
+    private fun setSpecificHandler(handlerBaseName: String, lazyBody: () -> String) =
+        setHandler(specificHandlers, handlerBaseName, lazyBody)
+
+    private fun setHandler(handlerSet: MutableMap<String, String>, handlerBaseName: String, lazyBody: () -> String) {
+        handlerSet[computeHandlerKey(handlerBaseName)] = lazyBody().trimIndent()
+    }
+
+    private fun computeHandlerKey(handlerBaseName: String) = handlerBaseName +
+            resourceName.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
 }
