a5fb6ee39dfa294a83869db91ad3da01b1026805
Alex Speranza <asperanza@imolainformatica.it>
2023-04-26 16:50:10 +0200

    feat: add the application of the plugin to the root project

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("it.imolainformatica.ll.ecg-plugin")
+}
+
+/*
+express {
+    baseAppSourceFolder = "./src"
+    mainRouteFileName = "main.ts"
+    routesFolder = "routes"
+    // routeSetterName = "routeSetter"
+    routesFolder = "routes"
+    controllerFolder = "controllers"
+    /*
+    route("/projects") {
+        resourceName = "project"
+        // import ...
+        getAll {
+            // ..
+        }
+
+        post {
+            // ..
+        }
+
+        getOne {
+            // ..
+        }
+
+        put {
+            // ..
+        }
+
+        delete {
+            // ..
+        }
+    }
+}
+*/
