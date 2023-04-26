8366a92599add2ff77fa933fbf83aa201d707584
Alex Speranza <asperanza@imolainformatica.it>
2023-04-26 17:53:11 +0200

    feat: add function 'route' to extension

--- buildSrc/src/main/kotlin/it/imolainformatica/ll/extension/ExpressCodeGeneratorExtension.kt
+++ buildSrc/src/main/kotlin/it/imolainformatica/ll/extension/ExpressCodeGeneratorExtension.kt
@@ -1,8 +1,24 @@
 package it.imolainformatica.ll.extension
 
+import it.imolainformatica.ll.spec.RouteSpec
+
 open class ExpressCodeGeneratorExtension {
     var baseAppSourceFolder = "./src"
     var mainRouteFileName = "main.ts"
     var routesFolder = "routes"
     var controllerFolder = "controllers"
+
+    var routeSpecs: MutableList<RouteSpec> = mutableListOf()
+        private set
+
+    fun route(path: String, action: RouteSpec.() -> Unit) =
+        routeSpecs.add(
+            RouteSpec(path)
+                .apply(action)
+                .also {
+                    if (it.resourceName.isEmpty()) {
+                        throw IllegalStateException("The resource name must be defined for path ${it.routePath}")
+                    }
+                }
+        )
 }
