31d822bfe997c0135406c12060e854199944b5f1
Alex Speranza <asperanza@imolainformatica.it>
2023-04-28 12:28:49 +0200

    feat: add functions to declare additional imports for a route

--- buildSrc/src/main/kotlin/it/imolainformatica/ll/spec/RouteSpec.kt
+++ buildSrc/src/main/kotlin/it/imolainformatica/ll/spec/RouteSpec.kt
@@ -11,11 +11,18 @@ open class RouteSpec(val routePath: String) {
     var specificHandlers: MutableMap<String, String> = mutableMapOf()
         private set
 
+    var sideEffectImports: MutableList<String> = mutableListOf()
+        private set
+
+    val additionalImports: MutableList<String> = mutableListOf()
+
     fun getAll(lazyBody: () -> String) = setGenericHandler("getEvery", lazyBody)
     fun post(lazyBody: () -> String) = setGenericHandler("post", lazyBody)
     fun getOne(lazyBody: () -> String) = setSpecificHandler("get", lazyBody)
     fun put(lazyBody: () -> String) = setSpecificHandler("put", lazyBody)
     fun delete(lazyBody: () -> String) = setSpecificHandler("delete", lazyBody)
+
+    fun importSideEffects(from: String) = sideEffectImports.add(from)
 
     private fun setGenericHandler(handlerBaseName: String, lazyBody: () -> String) =
         setHandler(genericHandlers, handlerBaseName, lazyBody)
@@ -29,4 +36,28 @@ open class RouteSpec(val routePath: String) {
 
     private fun computeHandlerKey(handlerBaseName: String) = handlerBaseName +
             resourceName.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
+
+    fun import(vararg symbols: String): ImportStatement = when(symbols.size) {
+        0 -> throw IllegalStateException("Import statements cannot have 0 arguments")
+        1 -> ImportSymbol(symbols[0])
+        else -> ImportList(listOf(*symbols))
+    }
+
+    infix fun ImportStatement.renamedAs(newName: String): ImportAllRenamed = run {
+        if (this !is ImportSymbol || this.symbol != "*") {
+            throw IllegalStateException("Cannot rename symbols other than '*'")
+        }
+        ImportAllRenamed(newName)
+    }
+
+    infix fun ImportStatement.from(reference: String) = additionalImports.add(when(this) {
+        is ImportAllRenamed -> "* as ${this.newName}"
+        is ImportList -> this.symbols.joinToString(", ", "{ ", " }")
+        is ImportSymbol -> this.symbol
+    } + " from \"${reference}\"")
+
+    sealed class ImportStatement
+    class ImportSymbol(val symbol: String) : ImportStatement()
+    class ImportList(val symbols: List<String>) : ImportStatement()
+    class ImportAllRenamed(val newName: String) : ImportStatement()
 }
